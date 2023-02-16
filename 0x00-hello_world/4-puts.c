baac9ae4da47648714ccf8a540d009d13e05347c
Hastycodea <hastycodeandreh@gmail.com>
2023-02-17 00:28:43 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry points
+ * Return: Always 0 (Success)
+*/
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
